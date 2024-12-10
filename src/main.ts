ad8fb65f32e45fdc1ddd1dc4c9404c4d256d2c3e
Hugo Averty <hugoaverty@gmail.com>
2024-12-10 10:06:06 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -91,7 +91,7 @@ WA.onInit().then(() => {
     WA.room.area.onEnter('popupZone').subscribe(() => {
         popupTest = WA.ui.openPopup("popupTest", "Test popup OK Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",[{
                 label: "Close",
-                className: "btn-secondary",
+                className: "success",
                 callback: (popup) => {
                     // Close the popup when the "Close" button is pressed.
                     popup.close();
